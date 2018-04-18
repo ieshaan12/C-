a343929f7e69b9067e3a82c52d43e689b72fa9bb
Ieshaan Saxena <31623825+ieshaan12@users.noreply.github.com>
2018-04-18 16:41:13 +0530

    Positiverootfinder

--- rootfinderxaxbc.cpp
+++ rootfinderxaxbc.cpp
@@ -2,15 +2,26 @@
 #include<math.h>
 using namespace std;
 int main()
-{double l=1,h=10,m;
+{double l=1,h=10,m,x;
+x=pow(10,299);
 float a,b,c;
+int f=0;
 cin>>a>>b>>c;
-while((pow(l,a)+pow(l,b)+c)*(pow(h,a)+pow(h,b)+c)>0)
+while((pow(l,a)+pow(l,b)+c)*(pow(h,a)+pow(h,b)+c)>0 && f==0)
 {l*=10;h*=10;
-cout<<l<<" "<<h<<endl;
+//cout<<l<<" "<<h<<endl;
+if(l>x)
+    {f=1;}
 }
+if(f==1)
+    {h=1;l=0;
+    while((pow(l,a)+pow(l,b)+c)*(pow(h,a)+pow(h,b)+c)>0)
+        {l/=10;h/=10;
+        //cout<<l<<" "<<h<<endl;
+        }
+    }    
 m=(l+h)/2;
-for (;(((pow(m,a)+pow(m,b)+c))*1000000)>1 || (((pow(m,a)+pow(m,b)+c))*1000000)<-1;m=(l+h)/2)
+for (;(((pow(m,a)+pow(m,b)+c))*100000000)>1 || (((pow(m,a)+pow(m,b)+c))*100000000)<-1;m=(l+h)/2)
     {
             if((pow(m,a)+pow(m,b)+c)>0)
                 {h=m;}
