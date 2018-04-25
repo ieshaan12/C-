557f0a84c19be8d39e4211256c949a07835eb8ae
Ieshaan Saxena <31623825+ieshaan12@users.noreply.github.com>
2018-04-25 18:00:02 +0530

    polynom_mak_eval.cpp
    This has two functions which do the function of making the Polynomial as per user's wants. Also evaluates the value of the formed polynomial.
    One bug is how to find the number of terms. Fairly easily solution would be to use vectors. Try that after learning STL

--- polynommaker.cpp
+++ polynommaker.cpp
@@ -1,29 +1,28 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
-int main()
-{int n;
-cout<<"Number of x,power terms you want to input"<<endl;
-cin>>n;
-int coeff[n],power[n];
-int c;
+int polynommaker(int n)             // This n will be the number of terms entered by the user.
+{//cout<<"Number of x,power terms you want to input"<<endl;
+//cin>>n
+int coeff[n],power[n];              //Find a solution for passing these array to a different function that will evaluate the polynmial.
 for(int i=0;i<n;i++)
 {cout<<"Coefficient for "<<(i+1)<<" term followed by its power"<<endl;
-    cin>>coeff[i]>>power[i];
+    cin>>coeff[i]>>power[i];            //Corresponding element in the array 
 }
-cout<<"Enter constant Term"<<endl;
-cin>>c;
 cout<<"Final equation is"<<endl;
 for(int i=0;i<n;i++)
-    {cout<<coeff[i]<<"*x^("<<power[i]<<") + ";
+    {cout<<coeff[i]<<"*x^("<<power[i]<<") ";
+        if(i!=n-1)
+            cout<<" + ";
     }
-    cout<<c<<endl<<"Enter value for which you want to find the value of this polynomial"<<endl;
-double x;
-cin>>x;
+    return 0;
+}
+
+double valuefinder(double val_x) 
+{//cout<<c<<endl<<"Enter value for which you want to find the value of this polynomial"<<endl;
 double value=0;
 for(int i=0;i<n;i++)
     {value+=coeff[i]*pow(x,power[i]);}
-    value=value+c;
-cout<<"Value of polynomial is "<<value<<endl;
-    return 0;
+    }
+    return value;
 }
